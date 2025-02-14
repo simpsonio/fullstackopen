4f2bcfad073ca3d9d2dfabe41b17be9fbf6239a1
simpsonio <198814643+simpsonio@users.noreply.github.com>
2025-02-14 15:45:20 +1300

    Add exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,21 +5,28 @@ const Header = ({text}) => {
 }
 
 const StatisticsLine = ({text, value}) => {
-  return  <div>{text} {value}</div>
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
 
 const Statistics = (props) => {
   if (props.all === 0) return <div>No feedback given</div>
 
   return (
-    <div>
+    <table>
+        <tbody>
       <StatisticsLine text={"good"} value={props.good}/>
       <StatisticsLine text={"neutral"} value={props.neutral}/>
       <StatisticsLine text={"bad"} value={props.bad}/>
       <StatisticsLine text={"all"} value={props.all}/>
       <StatisticsLine text={"average"} value={props.average}/>
       <StatisticsLine text={"positive"} value={props.positive}/>
-    </div>
+      </tbody>
+    </table>
   )
 }
 
