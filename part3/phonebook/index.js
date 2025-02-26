2630839731063e4212c1f4c1353ce65fc9ac3afa
simpsonio <198814643+simpsonio@users.noreply.github.com>
2025-02-27 12:56:44 +1300

    Add exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-let notes = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -25,7 +25,7 @@ let notes = [
     }
 ]
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(notes)
 })
 
@@ -38,6 +38,18 @@ app.get('/info', (request, response) => {
         `
         )
     )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
 })
 
 const PORT = 3001
