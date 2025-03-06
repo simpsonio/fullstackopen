64ef557957cdd4264116f6072978314b2b8a2650
simpsonio <198814643+simpsonio@users.noreply.github.com>
2025-03-06 18:06:59 +1300

    Add execises 3.16 3.17 and 3.18

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -12,25 +12,46 @@ app.use(cors())
 
 morgan.token("object", (request) => {
     return JSON.stringify(request.body);
-  });
-  app.use(
+});
+
+app.use(
     morgan(
-      ":method :url :status :res[content-length] - :response-time ms :object"
+        ":method :url :status :res[content-length] - :response-time ms :object"
     )
-  );
+);
+
+//fetch info page
+app.get('/info', (request, response) => {
+    Person.find({})
+        .then(persons => {
+            response.send(
+                (`
+                    <div>Phonebook has info for ${persons.length} people</div>
+                    <br>
+                    <div>${Date().toLocaleString()}</div>
+                `
+                )
+            )
+        })
+        .catch(error => next(error))
+})
 
 //fetch all persons
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
+        .catch(error => next(error))
 })
 
 //fetch one person
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
 //delete one person
@@ -39,6 +60,7 @@ app.delete('/api/persons/:id', (request, response) => {
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
 //add one person
@@ -54,10 +76,40 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
+
+//update person
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+const errorHandler = ( error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
